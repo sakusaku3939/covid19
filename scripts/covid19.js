474d0ec9154cdc10fea090025c39005fa0087e49
AokiYuki <yuukiyuuki223@gmail.com>
2020-07-13 12:09:30 +0900

    ボット名を入力したときにコマンド一覧を表示する処理追加

--- scripts/covid19.js
+++ scripts/covid19.js
@@ -1,20 +1,22 @@
 // Description:
 //   新型コロナウイルスに関する情報を取得してくれるbotです
 // Commands:
+//   covid19            - コマンド一覧を表示
 //   covid19 全国        - 全国の感染者数を表示
 //   covid19 (都道府県名) - 指定した都道府県の累計陽性者数を表示
-//   covid19 commands    - コマンド一覧を表示
-// 例)2020-7-9 時点の東京都の累計陽性者数は100人です
 'use strict'
 const json = require('./index.js')
 
 module.exports = robot => {
-    robot.respond(/commands/i, msg => {
-        const commands = "" +
+    robot.hear(/covid19$/g, msg => {
+        const description = "" +
+            "Description:\n" +
+            "```新型コロナウイルスに関する情報を取得してくれるbotです```\n" +
+            "Commands:\n" +
+            "```covid19             - コマンド一覧を表示\n" +
             "covid19 全国        - 全国の感染者数を表示\n" +
-            "covid19 (都道府県名) - 指定した都道府県の累計陽性者数を表示\n" +
-            "covid19 commands    - コマンド一覧を表示\n"
-        msg.send(commands)
+            "covid19 (都道府県名) - 指定した都道府県の累計陽性者数を表示```"
+        msg.send(description)
     })
     robot.respond(/(.+)/i, msg => {
         const str = msg.match[1].trim()
